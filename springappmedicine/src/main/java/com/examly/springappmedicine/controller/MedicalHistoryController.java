bc9ad62bd7580f36434e6b23f9a7b115838a0c85
BlackInkcoder <anirudhagarwal93@gmail.com>
2025-05-26 11:38:14 +0000

    4

--- springappmedicine/src/main/java/com/examly/springappmedicine/controller/MedicalHistoryController.java
+++ springappmedicine/src/main/java/com/examly/springappmedicine/controller/MedicalHistoryController.java
@@ -1,5 +1,35 @@
 package com.examly.springappmedicine.controller;
 
+@RequestController
+@RequestMapping("/api/medical-history")
 public class MedicalHistoryController {
     
+    @Autowired
+    private MedicalHistoryService medicalHistoryService;
+
+    @GetMapping
+    public List<MedicalHistory> getAll(){
+        return patientService.getAll();
+    }
+
+    @PostMapping
+    public MedicalHistory add(@RequestBody MedicalHistory mh){
+        return medicalHistoryService.add(mh);
+    }
+
+    @GetMapping("/{id}")
+    public MedicalHistory get(@PathVariable int id){
+        return medicalHistoryService.get(id);
+    }
+
+    @PutMapping("/{id}")
+    public MedicalHistory update(@PathVariable int id, @RequestBody MedicalHistory mh){
+        return medicalHistoryService.update(id, mh);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+        medicalHistoryService.delete(id);
+    }
+
 }
