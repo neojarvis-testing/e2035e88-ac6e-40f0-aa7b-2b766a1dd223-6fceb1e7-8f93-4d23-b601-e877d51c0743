55d4c9cf7b5412f97a31681f88376c1728d38b35
BlackInkcoder <anirudhagarwal93@gmail.com>
2025-05-26 11:17:03 +0000

    2

--- springappuser/src/main/java/com/examly/springappuser/service/UserServiceImpl.java
+++ springappuser/src/main/java/com/examly/springappuser/service/UserServiceImpl.java
@@ -1,5 +1,22 @@
 package com.examly.springappuser.service;
 
-public class UserServiceImpl {
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService {
     
+    @Autowired
+    private UserRepository userRepository;
+
+    public User register(User user){
+        return userRepository.save(user);
+    }
+
+    public String login(String email, String password){
+        Optional<user> user = userRepository.findAll().stream().filter(u -> u.getEmail().equals(email) && u.getPassword().equals(password)).findFirst();
+        if(user.isPresent()){
+            return UUID.randomUUID().toString();
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid Credentials");
+    }
 }
