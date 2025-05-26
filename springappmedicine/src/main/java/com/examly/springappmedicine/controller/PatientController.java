584adb7bdfb05777ec4dd62fc61427e687ca2360
BlackInkcoder <anirudhagarwal93@gmail.com>
2025-05-26 11:30:43 +0000

    3

--- springappmedicine/src/main/java/com/examly/springappmedicine/controller/PatientController.java
+++ springappmedicine/src/main/java/com/examly/springappmedicine/controller/PatientController.java
@@ -1,4 +1,35 @@
 package com.examly.springappmedicine.controller;
+
+@RequestController
+@RequestMapping("/api/patients")
 public class PatientController {
     
+    @Autowired
+    private PatientService patientService;
+
+    @GetMapping
+    public List<Patient> getAllPatients(){
+        return patientService.getAllPatients();
+    }
+
+    @PostMapping
+    public Patient addPatient(@RequestBody Patient patient){
+        return patientService.addPatient(patient);
+    }
+
+    @GetMapping("/{id}")
+    public Patient getPatient(@PathVariable int id){
+        return patientService.getPatientById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Patient updatePatient(@PathVariable int id, @RequestBody Patient updatedPatient){
+        return patientService.updatePatient(id, updatedPatient);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePatient(@PathVariable int id){
+        patientService.deletePatient(id);
+    }
+
 }
