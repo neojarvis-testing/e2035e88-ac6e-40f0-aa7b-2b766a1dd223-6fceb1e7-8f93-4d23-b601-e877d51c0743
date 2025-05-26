dbbf5ed3742691ea7602bb95b9098624ffbf8f64
BlackInkcoder <anirudhagarwal93@gmail.com>
2025-05-26 12:04:26 +0000

    6

--- springappmedicine/src/main/java/com/examly/springappmedicine/controller/NotificationController.java
+++ springappmedicine/src/main/java/com/examly/springappmedicine/controller/NotificationController.java
@@ -1,4 +1,25 @@
 package com.examly.springappmedicine.controller;
+
+@RequestController
+@RequestMapping("/api/notifications")
 public class NotificationController {
     
+    @Autowired
+    private NotificationService notificationService;
+
+    @GetMapping
+    public List<Notification> getAll(){
+        return notificationService.getAll();
+    }
+
+    @PostMapping
+    public Notification add(@RequestBody Notification notification){
+        return notificationService.add(notification);
+    }
+
+    @PutMapping("/{id}")
+    public Notification update(@PathVariable int id, @RequestBody Notification notification){
+        return notificationService.update(id, notification);
+    }
+
 }
